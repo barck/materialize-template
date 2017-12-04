86c4797a7830f4e2894ae12c9ec45eedfe3ae440
barck94 <artiv09@gmail.com>
2017-12-04 15:15:26 +0500

    saturn on materialize

---
+++ public/js/scripts.js
@@ -0,0 +1,17 @@
+$(".button-collapse").sideNav();
+$('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        hover: true, // Activate on hover
+        belowOrigin: true, // Displays dropdown below the button
+
+    }
+);
+
+$('.parallax').parallax();
+$('.carousel').carousel({
+    dist: 0,
+    padding: 50,
+    indicators: true,
+
+});
